d7ff4c9b11191d3fde25bc72fd0d4e7dbbf4052c
Son Nguyen <sonnguyen@Sons-MacBook-Pro.local>
2017-03-07 14:18:54 -0800

    Fibonacci

---
+++ src/main/java/CtCI/chapter08/Fibonacci.java
@@ -0,0 +1,47 @@
+package CtCI.chapter08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by sonnguyen on 3/7/17.
+ */
+public class Fibonacci {
+
+    private static Map<Integer, Integer> memorization = new HashMap<>();
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println(fibonacci(40) + " = " + (System.currentTimeMillis() - start) + " ms");
+
+        start = System.currentTimeMillis();
+        System.out.println(fibonacciDynamically(40) + " = " + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    public static int fibonacci(int position) {
+        if (position == 0) {
+            return 0;
+        } else if (position == 1) {
+            return 1;
+        }
+
+        return fibonacci(position - 1) + fibonacci(position - 2);
+    }
+
+    public static int fibonacciDynamically(int position) {
+        if (position == 0) {
+            return 0;
+        } else if (position == 1) {
+            return 1;
+        }
+
+        if (memorization.containsKey(position)) {
+            return memorization.get(position);
+        }
+
+        int result = fibonacciDynamically(position - 1) +
+                fibonacciDynamically(position - 2);
+        memorization.put(position, result);
+        return result;
+    }
+}
