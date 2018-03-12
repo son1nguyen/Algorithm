23b2b0d06545ee85086581a4ad20e4a17627346b
Son Nguyen <sonnguyen@Sons-MacBook-Pro.local>
2018-03-12 01:30:23 -0700

    Subsets

--- src/main/java/LeetCode/Subsets.java
+++ src/main/java/LeetCode/Subsets.java
@@ -8,7 +8,25 @@ import java.util.List;
  * Created by sonnguyen on 1/25/18.
  */
 public class Subsets {
-
+    /**
+     * Given a set of distinct integers, nums, return all possible subsets (the power set).
+     * <p>
+     * Note: The solution set must not contain duplicate subsets.
+     * <p>
+     * For example,
+     * If nums = [1,2,3], a solution is:
+     * <p>
+     * [
+     * [3],
+     * [1],
+     * [2],
+     * [1,2,3],
+     * [1,3],
+     * [2,3],
+     * [1,2],
+     * []
+     * ]
+     */
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3};
         List<List<Integer>> result = subsets(nums);
